6e6246fc61b3570629918bd09585bd005d2a7ef8
learnProject <leanProject@example.com>
2025-03-21 21:44:29 +0900

    feat: add BookingSerializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -4,4 +4,9 @@ from .models import Menu, Booking
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = "__all__"
+        fields = ['title', 'price', 'inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
