66e8538b6c289cfc7176c5b9e36e5bf4a12859bb
learnProject <leanProject@example.com>
2025-03-21 21:32:37 +0900

    feat: add MenuItemsView, SingleMenuItemView and path()

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rest_framework import generics
+
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+# API views
+class MenuItemsView(generics.ListCreateView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
